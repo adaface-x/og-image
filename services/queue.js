8a496200c9152cface3f8bce73b4cea7171e58e5
Sanjana Kumari <sanjana01999@gmail.com>
2023-09-08 15:52:21 +0530

    redis bug fix

--- services/queue.js
+++ services/queue.js
@@ -2,8 +2,7 @@ const Queue = require("bull");
 
 var clearImagesQueue = new Queue(
 	"clear-image-queue",
-	`${process.env.OG_IMAGE_REDIS_URL}2`,
-	{ redis: { tls: true, enableTLSForSentinelMode: false } }
+	`${process.env.OG_IMAGE_REDIS_URL}2`
 );
 
 module.exports = {
