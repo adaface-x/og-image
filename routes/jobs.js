0d91c0a7db87d2778c1ece4bb6b459979773ca23
Sanjana Kumari <sanjana01999@gmail.com>
2023-11-29 13:31:25 +0530

    removed job to remove dev profile

--- routes/jobs.js
+++ routes/jobs.js
@@ -44,21 +44,6 @@ clearImagesQueue.process("clear-image-queue", async (job, done) => {
 			}
 		});
 
-		//find dev profile files with name starting with "puppeteer_dev*" and delete them
-		command = `find / -type d -name "puppeteer_dev*" | xargs rm -rf`;
-		exec(command, (error, stdout, stderr) => {
-			if (error) {
-				console.error(`Error executing command: ${error.message}`);
-				return;
-			}
-
-			if (stderr) {
-				console.error(`Command stderr: ${stderr}`);
-				return;
-			}
-
-			console.log(`Command stdout: ${stdout}`);
-		});
 		done(null, {
 			success: true,
 		});
